41274ddfd427c5b1e92d9a309023f8cd60b1e9cb
BenjaminKleppe <45095665+BenjaminKleppe@users.noreply.github.com>
2022-03-31 17:01:23 +0200

    x

--- NFCReader/src/main/java/com/example/demo/nfcReader/NFCReaderRepository.java
+++ NFCReader/src/main/java/com/example/demo/nfcReader/NFCReaderRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.nfcReader;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
