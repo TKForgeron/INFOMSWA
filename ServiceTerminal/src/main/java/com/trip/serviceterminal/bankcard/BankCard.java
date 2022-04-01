163d9c0fd1ac881f677a308d128a8d27a5f7290f
TKForgeron <trollololollolol.js@gmail.com>
2022-04-01 22:56:22 +0200

    bankcard management in serviceterminal kinda works

--- ServiceTerminal/src/main/java/com/trip/serviceterminal/bankcard/BankCard.java
+++ ServiceTerminal/src/main/java/com/trip/serviceterminal/bankcard/BankCard.java
@@ -1,15 +1,9 @@
 package com.trip.serviceterminal.bankcard;
 
-import javax.persistence.*;
+// import javax.persistence.*;
 import java.time.LocalDate;
 
-@Entity
-@Table
 public class BankCard {
-    @Id
-    @SequenceGenerator(name = "nfc_sequence", sequenceName = "nfc_sequence", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "nfc_sequence")
-    private Long uuid;
     private LocalDate expiryDate;
     private Integer nfcId;
     private String iban;
@@ -21,14 +15,6 @@ public class BankCard {
         this.expiryDate = expiryDate;
         this.nfcId = nfcId;
         this.iban = iban;
-    }
-
-    public Long getUuid() {
-        return this.uuid;
-    }
-
-    public void setUuid(Long uuid) {
-        this.uuid = uuid;
     }
 
     public LocalDate getExpiryDate() {
@@ -55,11 +41,6 @@ public class BankCard {
         this.iban = iban;
     }
 
-    public BankCard uuid(Long uuid) {
-        setUuid(uuid);
-        return this;
-    }
-
     public BankCard expiryDate(LocalDate expiryDate) {
         setExpiryDate(expiryDate);
         return this;
@@ -78,7 +59,6 @@ public class BankCard {
     @Override
     public String toString() {
         return "{" +
-                " uuid='" + getUuid() + "'" +
                 ", expiryDate='" + getExpiryDate() + "'" +
                 ", nfcId='" + getNfcId() + "'" +
                 ", iban='" + getIban() + "'" +
