b18abce938041a99ae3e6ecd4c196b415643986b
TKForgeron <trollololollolol.js@gmail.com>
2022-04-06 22:50:22 +0200

    rm stupid import

--- SubscriptionService/src/main/java/com/trip/subscriptionservice/subscription/SubscriptionRepository.java
+++ SubscriptionService/src/main/java/com/trip/subscriptionservice/subscription/SubscriptionRepository.java
@@ -1,6 +1,5 @@
 package com.trip.subscriptionservice.subscription;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
--- SubscriptionService/src/main/java/com/trip/subscriptionservice/subscription/SubscriptionService.java
+++ SubscriptionService/src/main/java/com/trip/subscriptionservice/subscription/SubscriptionService.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import jdk.jshell.JShell.Subscription;
-
 import java.util.List;
 import java.util.Optional;
 
